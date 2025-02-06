10c772387367c8081b034a0a7c8fd98a62ceb7bc
nhatnguyen150100 <nhatnguyen150100@gmail.com>
2025-02-06 11:40:39 +0700

    fix

--- src/models/user.js
+++ src/models/user.js
@@ -17,6 +17,9 @@ const userSchema = new Schema(
       default: false,
       required: true,
     },
+    arrive: {
+      type: String,
+    },
     message: {
       type: String,
       default: "",
